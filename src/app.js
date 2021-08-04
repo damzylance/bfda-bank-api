9990f75e02d9357aa2af2cf0bd1fbedc7b79678a
damzylance <32623753+damzylance@users.noreply.github.com>
2021-08-05 00:05:13 +0100

    Updated Babel, ESling and Prettier configuration

--- src/app.js
+++ src/app.js
@@ -1,22 +1,15 @@
-let { json } = require('express');
-let express = require('express')
-let fs = require('fs')
-let bcrypt = require("bcryptjs")
-const {rootRouter:useApi} = require('./routes')
+const express = require('express')
+const { rootRouter: useApi } = require('./routes')
 
 const app = express()
-let PORT = 3000;
-
+const PORT = 3000
 
 app.use(express.json())
 app.use(useApi)
 
-
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send(`app is running at ${PORT}`)
 })
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`app is running at`)
-    
 })
-
